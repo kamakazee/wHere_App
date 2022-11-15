e0b560ea433cf0e9374cd11dc0dbd5071d9cc4d8
Kamakazee <cam_va@yahoo.co.uk>
2022-11-15 15:32:51 +0000

    GetImages through endpoint implimented

--- db/conn2.js
+++ db/conn2.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(require("../routes/record2.js"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+  
 
 const Db = process.env.ATLAS_URI;
 
